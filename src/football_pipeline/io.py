ed88a7ca55b6f07fc4172f63e3f92d2bf2706cdb
spencerduberry <spencerduberry@hotmail.co.uk>
2025-02-10 08:21:37 +0000

    Fix needed

--- src/football_pipeline/io.py
+++ src/football_pipeline/io.py
@@ -1,1 +1,31 @@
-"""Module for input-output functions."""
+import os
+import yaml
+
+
+def read_yaml(path: str) -> dict:
+    """
+    Loads a YAML file from the given path and returns its contents as a dict.
+
+    Args:
+        path: the path to the YAML file.
+
+    Returns:
+        A dict representation of the YAML file or an empty dict on error.
+    """
+    try:
+        abs_path = os.path.abspath(path)
+
+        with open(path, "r") as file:
+            data = yaml.safe_load(file)
+
+        if data is None:
+            print(
+                f"Warning: {abs_path} is empty or invalid YAML. Returning empty dict."
+            )
+            return {}
+
+        return data
+
+    except (yaml.YAMLError, Exception) as e:
+        print(f"Error loading YAML from {path}: {e}")
+        return {}
---
+++ tests/test_io.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import pytest
+
+import os
+print(f"Current working directory: {os.getcwd()}")
+
+from src.football_pipeline.io import read_yaml
+
+current_file_path = Path(__file__).resolve()
+
+MOCK_DATA_DIR = current_file_path.parent / "mock_data" / "mock_yamls"
+
+valid_yaml_path = MOCK_DATA_DIR / "valid.yaml"
+invalid_yaml_path = MOCK_DATA_DIR / "invalid.yaml"
+
+
+@pytest.mark.parametrize(
+    "test_file, expected_result",
+    [
+        (
+            valid_yaml_path,
+            {
+                "league": {
+                    "backseries": False,
+                    "players": ["some player", "some other player"],
+                }
+            },
+        ),
+        (invalid_yaml_path, {}),
+    ],
+)
+def test_read_yaml(test_file, expected_result):
+    """Tests the read_yaml function using a sample file."""
+    result = read_yaml(test_file)
+    print(f"Result for {test_file}: {result}")
+    assert result == expected_result
