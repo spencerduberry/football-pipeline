15ecbde52b70a353de49bc18621e80d79cc75323
spencerduberry <spencerduberry@hotmail.co.uk>
2025-04-06 21:55:44 +0100

    feat: validate teams table.

--- src/football_pipeline/data_structures.py
+++ src/football_pipeline/data_structures.py
@@ -1,56 +1,15 @@
 import attrs
-from attrs.validators import in_, instance_of
-
-teams = [
-    "Arsenal",
-    "Aston Villa",
-    "Bournemouth",
-    "Brentford",
-    "Brighton",
-    "Chelsea",
-    "Crystal Palace",
-    "Everton",
-    "Fulham",
-    "Ipswich",
-    "Leicester",
-    "Liverpool",
-    "Man City",
-    "Man Utd",
-    "Newcastle",
-    "Nott'm Forest",
-    "Southampton",
-    "Spurs",
-    "West Ham",
-    "Wolves",
-]
-
-
-def is_length_3(instance, attribute, value):
-    if not isinstance(value, str):
-        raise TypeError(
-            f"'{attribute.name}' must be a string (got {type(value).__name__})"
-        )
-    if len(value) != 3:
-        raise ValueError(
-            f"'{attribute.name}' must be exactly 3 characters long (got {len(value)})"
-        )
-
-
-def all_caps(instance, attribute, value):
-    if not isinstance(value, str):
-        raise TypeError(
-            f"'{attribute.name}' must be a string (got {type(value).__name__})"
-        )
-    if not value.isupper():
-        raise ValueError(
-            f"'{attribute.name}' must contain only uppercase letters (got '{value}')"
-        )
+from attrs.validators import gt, in_, instance_of, matches_re, max_len, min_len
 
 
 @attrs.define
 class TeamsTable:
-    team_id: int = attrs.field(validator=[instance_of(int), in_(range(1, 21))])
-    name: str = attrs.field(validator=[instance_of(str), in_(teams)])
-    position: int = attrs.field(validator=[instance_of(int), in_(range(1, 21))])
-    short_name: str = attrs.field(validator=[instance_of(str), is_length_3, all_caps])
-    strength: int = attrs.field(validator=[instance_of(int)])
+    team_id: int = attrs.field(validator=[instance_of(int), gt(0)], converter=int)
+    name: str = attrs.field(validator=[instance_of(str), min_len(2), max_len(100)])
+    position: int = attrs.field(
+        validator=[instance_of(int), in_(range(1, 21))], converter=int
+    )
+    short_name: str = attrs.field(
+        validator=[instance_of(str), matches_re("[A-Z]{3}")], converter=[str, str.upper]
+    )
+    strength: int = attrs.field(validator=[instance_of(int)], converter=int)
