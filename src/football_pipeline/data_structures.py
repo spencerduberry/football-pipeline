e9c30cab3c782c01334064b45547fbe868f11325
spencerduberry <spencerduberry@hotmail.co.uk>
2025-04-07 21:49:04 +0100

    Fix: standardise bound methods

--- src/football_pipeline/data_structures.py
+++ src/football_pipeline/data_structures.py
@@ -1,13 +1,13 @@
 import attrs
-from attrs.validators import gt, in_, instance_of, matches_re, max_len, min_len
+from attrs.validators import ge, instance_of, lt, matches_re, max_len, min_len
 
 
 @attrs.define
 class BronzeTeams:
-    team_id: int = attrs.field(validator=[instance_of(int), gt(0)], converter=int)
+    team_id: int = attrs.field(validator=[instance_of(int), ge(1)], converter=int)
     name: str = attrs.field(validator=[instance_of(str), min_len(2), max_len(100)])
     position: int = attrs.field(
-        validator=[instance_of(int), in_(range(1, 21))], converter=int
+        validator=[instance_of(int), ge(1), lt(21)], converter=int
     )
     short_name: str = attrs.field(
         validator=[instance_of(str), matches_re("[A-Z]{3}")], converter=[str, str.upper]
@@ -16,37 +16,37 @@ class BronzeTeams:
 
 @attrs.define
 class BronzeEvent:
-    event_id: int = attrs.field(validator=[instance_of(int), gt(0)], converter=int)
+    event_id: int = attrs.field(validator=[instance_of(int), ge(1)], converter=int)
     event: str = attrs.field(validator=[instance_of(str), min_len(2), max_len(100)])
 
 
 @attrs.define
 class BronzePlayerMatchStats:
-    player_id: int = attrs.field(validator=[instance_of(int), gt(0)], converter=int)
-    fixture_id: int = attrs.field(validator=[instance_of(int), gt(0)], converter=int)
+    player_id: int = attrs.field(validator=[instance_of(int), ge(1)], converter=int)
+    fixture_id: int = attrs.field(validator=[instance_of(int), ge(1)], converter=int)
     minutes_played: int = attrs.field(
-        validator=[instance_of(int), gt(-1)], converter=int
+        validator=[instance_of(int), ge(0)], converter=int
     )
     influence: float = attrs.field(
-        validator=[instance_of(float), gt(-1)], converter=float
+        validator=[instance_of(float), ge(0)], converter=float
     )
     creativity: float = attrs.field(
-        validator=[instance_of(float), gt(-1)], converter=float
+        validator=[instance_of(float), ge(0)], converter=float
     )
-    threat: float = attrs.field(validator=[instance_of(float), gt(-1)], converter=float)
+    threat: float = attrs.field(validator=[instance_of(float), ge(0)], converter=float)
     ict_index: float = attrs.field(
-        validator=[instance_of(float), gt(-1)], converter=float
+        validator=[instance_of(float), ge(0)], converter=float
     )
-    starts: int = attrs.field(validator=[instance_of(int), gt(-1)], converter=int)
+    starts: int = attrs.field(validator=[instance_of(int), ge(0)], converter=int)
     expeected_goals: float = attrs.field(
-        validator=[instance_of(float), gt(-1)], converter=float
+        validator=[instance_of(float), ge(0)], converter=float
     )
     expected_assists: float = attrs.field(
-        validator=[instance_of(float), gt(-1)], converter=float
+        validator=[instance_of(float), ge(0)], converter=float
     )
     expected_goal_involvements: float = attrs.field(
-        validator=[instance_of(float), gt(-1)], converter=float
+        validator=[instance_of(float), ge(0)], converter=float
     )
     expected_goals_conceded: float = attrs.field(
-        validator=[instance_of(float), gt(-1)], converter=float
+        validator=[instance_of(float), ge(0)], converter=float
     )
