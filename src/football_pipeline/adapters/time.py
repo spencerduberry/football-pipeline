bcceed4028388dcef0fb44787d87c383027b2042
Christian Matthews <c_matthews90@live.com>
2025-07-15 20:56:24 +0100

    test: API tests

---
+++ src/football_pipeline/adapters/time.py
@@ -0,0 +1,20 @@
+import datetime
+import uuid
+
+
+def time_now(date_format: str = "%Y%m%d - %H%M%S") -> str:
+    return datetime.datetime.now(datetime.timezone.utc).strftime(date_format)
+
+
+def fake_time_now(date_format: str = "%Y%m%d - %H%M%S") -> str:
+    return datetime.datetime(
+        2025, 4, 25, 23, 23, 55, tzinfo=datetime.timezone.utc
+    ).strftime(date_format)
+
+
+def new_guid() -> str:
+    return str(uuid.uuid4())
+
+
+def fake_new_guid() -> str:
+    return "123432424-absdbfdf"
