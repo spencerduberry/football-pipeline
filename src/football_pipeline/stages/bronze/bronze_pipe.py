91b1f659a41b4c6363f847e67addc7972fc54cff
spencerduberry <spencerduberry@hotmail.co.uk>
2025-11-04 15:28:27 +0000

    fix: implement Ed's clean code refactor

--- src/football_pipeline/stages/bronze/bronze_pipe.py
+++ src/football_pipeline/stages/bronze/bronze_pipe.py
@@ -5,20 +5,9 @@ from src.football_pipeline.stages.bronze.data_structures import (
     BronzeSchema,
 )
 
-# prints raw fixtures table
-pd.set_option("display.max_columns", None)
-pd.set_option("display.max_colwidth", None)
-io = IOWrapper()
-df = io.read(
-    r"C:\Users\spenc\github\football-pipeline\data\00_raw\fantasy_premierleague_com_api_fixtures\20251007_191523.parquet",
-    FileType.PARQUET,
-)
-df
-
 
 # works on player, teams, fixtures tables
-def generic_bronze_transform(config_path: str, table: BronzeSchema):
-    io = IOWrapper()
+def generic_bronze_transform(io: IOWrapper, config_path: str, table: BronzeSchema):
     df = io.read(config_path, FileType.PARQUET)
     valid, invalid = [], []
     rows = df.to_dict("records")
@@ -37,15 +26,6 @@ def generic_bronze_transform(config_path: str, table: BronzeSchema):
 
 
 def bronze_stats_event_transform(df: pd.DataFrame) -> pd.DataFrame:
-    raw_columns = df[["id", "team_h", "team_a", "stats"]]
-    exploded_columns = raw_columns.explode("stats").reset_index(drop=True)
-    serialised_columns = (
-        exploded_columns["stats"].apply(pd.Series).reset_index(drop=True)
-    )
-    concat_df = pd.concat([exploded_columns, serialised_columns], axis=1)
-    a_table = concat_df.explode("a")
-    h_table = concat_df.explode("h")
-
     def transform_stats_list(stats_df: pd.DataFrame, team_columns: list):
         team, players = team_columns
         stats = (
@@ -65,6 +45,15 @@ def bronze_stats_event_transform(df: pd.DataFrame) -> pd.DataFrame:
         )
 
         return output
+
+    raw_columns = df[["id", "team_h", "team_a", "stats"]]
+    exploded_columns = raw_columns.explode("stats").reset_index(drop=True)
+    serialised_columns = (
+        exploded_columns["stats"].apply(pd.Series).reset_index(drop=True)
+    )
+    concat_df = pd.concat([exploded_columns, serialised_columns], axis=1)
+    a_table = concat_df.explode("a")
+    h_table = concat_df.explode("h")
 
     final_a_table = transform_stats_list(a_table, ["team_a", "a"])
     final_h_table = transform_stats_list(h_table, ["team_h", "h"])
