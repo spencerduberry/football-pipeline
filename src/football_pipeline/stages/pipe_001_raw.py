d7bd47460012b9b605f249b9ba381dbbf0190b0e
Chris Matthews <c_matthews90@live.com>
2025-11-17 19:50:06 +0000

    feat: element_summary api added into config and run_raw_layer

--- src/football_pipeline/stages/pipe_001_raw.py
+++ src/football_pipeline/stages/pipe_001_raw.py
@@ -21,28 +21,95 @@ def run_raw_layer(config_path: str, repo: Repo) -> dict[str, bool]:
     successes = {}
 
     for k, v in config.get("endpoints", {}).items():
-        path = v.get("path", "")
-        keys = v.get("keys", [])
-        api_response = repo.io.read(path, FileType.FOOTBALL_API)
-        repo.logger.info(msg="successfully received message.", path=path, keys=keys)
-        res = convert_data_to_df(api_response, path, keys)
-
-        if isinstance(res, list):
-            repo.logger.error(df=res, err="Retrieved object was a list.")
-            raise ValueError(res)
-
-        res = {
-            k: add_ingestion_columns(v, batch_guid, date_time_str)
-            for k, v in res.items()
-        }
-
-        for k, v in res.items():
-            file_save_path = f"{save_path}/{k}/{date_time_str}.parquet"
-            repo.fs.create_dir(os.path.dirname(file_save_path))
-            successes[file_save_path] = repo.io.write(
-                file_save_path, v, FileType.PARQUET
+        if k == "element_summary":
+            successes.update(
+                process_player_endpoint(
+                    repo,
+                    k,
+                    v,
+                    batch_guid,
+                    date_time_str,
+                    save_path,
+                    config.get("player_id", {}).get("min", 1),
+                    config.get("player_id", {}).get("max", 748),
+                )
+            )
+        else:
+            successes.update(
+                process_endpoint(repo, k, v, batch_guid, date_time_str, save_path)
             )
 
+    return successes
+
+
+def process_endpoint(
+    repo: Repo,
+    endpoint: str,
+    endpoint_details: dict,
+    batch_guid: str,
+    date_time_str: str,
+    save_path: str,
+) -> dict[str, bool]:
+    successes = {}
+    path = endpoint_details.get("path", "")
+    keys = endpoint_details.get("keys", [])
+    api_response = repo.io.read(path, FileType.FOOTBALL_API)
+    repo.logger.info(msg="successfully received message.", path=path, keys=keys)
+    res = convert_data_to_df(api_response, path, keys)
+
+    if isinstance(res, list):
+        repo.logger.error(df=res, err="Retrieved object was a list.")
+        raise ValueError(res)
+
+    res = {
+        k: add_ingestion_columns(v, batch_guid, date_time_str) for k, v in res.items()
+    }
+
+    for endpoint, endpoint_details in res.items():
+        file_save_path = f"{save_path}/{endpoint}/{date_time_str}.parquet"
+        repo.fs.create_dir(os.path.dirname(file_save_path))
+        successes[file_save_path] = repo.io.write(
+            file_save_path, endpoint_details, FileType.PARQUET
+        )
+    return successes
+
+
+def process_player_endpoint(
+    repo: Repo,
+    endpoint: str,
+    endpoint_details: dict,
+    batch_guid: str,
+    date_time_str: str,
+    save_path: str,
+    min_player_id: int,
+    max_player_id: int,
+) -> dict[str, bool]:
+    successes = {}
+    path = endpoint_details.get("path", "")
+    keys = endpoint_details.get("keys", [])
+    for pid in range(min_player_id, max_player_id + 1):
+        try:
+            player_path = path.format(id=pid)
+            api_response = repo.io.read(player_path, FileType.FOOTBALL_API)
+        except Exception as e:
+            repo.logger.error(player_path=player_path, err=e)
+        else:
+            res = convert_data_to_df(api_response, path, keys)
+            if isinstance(res, list):
+                repo.logger.error(df=res, err="Retrieved object was a list.")
+                raise ValueError(res)
+
+            res = {
+                k: add_ingestion_columns(v, batch_guid, date_time_str)
+                for k, v in res.items()
+            }
+
+            for endpoint, endpoint_details in res.items():
+                file_save_path = f"{save_path}/{endpoint}/{date_time_str}.parquet"
+                repo.fs.create_dir(os.path.dirname(file_save_path))
+                successes[file_save_path] = repo.io.write(
+                    file_save_path, endpoint_details, FileType.PARQUET
+                )
     return successes
 
 
