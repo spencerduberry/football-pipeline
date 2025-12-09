2b923541adb7247b1f3f2ba9f093b5a043762712
spencerduberry <spencerduberry@hotmail.co.uk>
2025-12-09 17:00:29 +0000

    feat: add validator class tests

--- tests/stages/bronze/test_bronze_pipe.py
+++ tests/stages/bronze/test_bronze_pipe.py
@@ -4,7 +4,13 @@ import pandas as pd
 import pytest
 
 from football_pipeline.stages.bronze.bronze_pipe import generic_bronze_transform
-from football_pipeline.stages.bronze.data_structures import BronzePlayer, BronzeTeams
+from football_pipeline.stages.bronze.data_structures import (
+    BronzeEvent,
+    BronzeFixture,
+    BronzePlayer,
+    BronzeStats,
+    BronzeTeams,
+)
 
 TEAM_INPUT_DF = pd.DataFrame(
     [
@@ -190,6 +196,200 @@ PLAYER_EXPECTED_RESULT = (
     ),
 )
 
+EVENT_INPUT_DF = pd.DataFrame(
+    [
+        {"event_id": 0, "event": "red_card"},
+        {"event_id": 1, "event": "r"},
+        {"event_id": 2, "event": "yellow_card"},
+    ]
+)
+
+
+EVENT_EXPECTED_RESULT = (
+    pd.DataFrame(
+        [
+            {"event_id": 2, "event": "yellow_card"},
+        ]
+    ),
+    pd.DataFrame(
+        [
+            {
+                "event_id": 0,
+                "event": "red_card",
+                "error": "'event_id' must be >= 1: 0",
+            },
+            {
+                "event_id": 1,
+                "event": "r",
+                "error": "Length of 'event' must be >= 2: 1",
+            },
+        ]
+    ),
+)
+
+STATS_INPUT_DF = pd.DataFrame(
+    [
+        {
+            "event_id": 0,
+            "team_id": 1,
+            "fixture_id": 2,
+            "player_id": 3,
+        },
+        {
+            "event_id": 1,
+            "team_id": 2,
+            "fixture_id": 3,
+            "player_id": 4,
+        },
+    ]
+)
+
+
+STATS_EXPECTED_RESULT = (
+    pd.DataFrame(
+        [
+            {
+                "event_id": 1,
+                "team_id": 2,
+                "fixture_id": 3,
+                "player_id": 4,
+            },
+        ]
+    ),
+    pd.DataFrame(
+        [
+            {
+                "event_id": 0,
+                "team_id": 1,
+                "fixture_id": 2,
+                "player_id": 3,
+                "error": "'event_id' must be >= 1: 0",
+            },
+        ]
+    ),
+)
+
+FIXTURE_INPUT_DF = pd.DataFrame(
+    [
+        {
+            "id": 1,
+            "started": True,
+            "finished": False,
+            "kickoff_time": "1999-12-31",
+            "team_h": 1,
+            "team_a": 2,
+            "team_h_difficulty": 1,
+            "team_a_difficulty": 2,
+        },
+        {
+            "id": 0,
+            "started": True,
+            "finished": False,
+            "kickoff_time": "1999-12-31",
+            "team_h": 1,
+            "team_a": 2,
+            "team_h_difficulty": 1,
+            "team_a_difficulty": 2,
+        },
+        {
+            "id": 1,
+            "started": True,
+            "finished": "False",
+            "kickoff_time": "1999-12-31",
+            "team_h": 1,
+            "team_a": 2,
+            "team_h_difficulty": 1,
+            "team_a_difficulty": 2,
+        },
+        {
+            "id": 1,
+            "started": True,
+            "finished": False,
+            "kickoff_time": "19999-12-31",
+            "team_h": 1,
+            "team_a": 2,
+            "team_h_difficulty": 1,
+            "team_a_difficulty": 2,
+        },
+        {
+            "id": 1,
+            "started": True,
+            "finished": False,
+            "kickoff_time": "1999-12-31",
+            "team_h": 1,
+            "team_a": 2,
+            "team_h_difficulty": -1,
+            "team_a_difficulty": 2,
+        },
+    ]
+)
+
+
+FIXTURE_EXPECTED_RESULT = (
+    pd.DataFrame(
+        [
+            {
+                "id": 1,
+                "started": True,
+                "finished": False,
+                "kickoff_time": datetime.date(1999, 12, 31),
+                "team_h": 1,
+                "team_a": 2,
+                "team_h_difficulty": 1,
+                "team_a_difficulty": 2,
+            },
+        ]
+    ),
+    pd.DataFrame(
+        [
+            {
+                "id": 0,
+                "started": True,
+                "finished": False,
+                "kickoff_time": "1999-12-31",
+                "team_h": 1,
+                "team_a": 2,
+                "team_h_difficulty": 1,
+                "team_a_difficulty": 2,
+                "error": "'id' must be >= 1: 0",
+            },
+            {
+                "id": 1,
+                "started": True,
+                "finished": "False",
+                "kickoff_time": "1999-12-31",
+                "team_h": 1,
+                "team_a": 2,
+                "team_h_difficulty": 1,
+                "team_a_difficulty": 2,
+                "error": "'finished' must be <class 'bool'> (got 'False' that is a <class 'str'>).",
+            },
+            {
+                "id": 1,
+                "started": True,
+                "finished": False,
+                "kickoff_time": "19999-12-31",
+                "team_h": 1,
+                "team_a": 2,
+                "team_h_difficulty": 1,
+                "team_a_difficulty": 2,
+                "error": "time data '19999-12-3' does not match format '%Y-%m-%d'",
+            },
+            {
+                "id": 1,
+                "started": True,
+                "finished": False,
+                "kickoff_time": "1999-12-31",
+                "team_h": 1,
+                "team_a": 2,
+                "team_h_difficulty": -1,
+                "team_a_difficulty": 2,
+                "error": "'team_h_difficulty' must be >= 0: -1",
+            },
+        ]
+    ),
+)
+
 
 @pytest.mark.parametrize(
     "inp_df, validator_cls, expected_result",
@@ -206,18 +406,24 @@ PLAYER_EXPECTED_RESULT = (
             PLAYER_EXPECTED_RESULT,
             id="Ensure separates BronzePlayerFixture valid and invalid records",
         ),
-        # pytest.param(
-        #     TEAM_INPUT_DF,
-        #     BronzeTeams,
-        #     TEAM_EXPECTED_RESULT,
-        #     id="Ensure separates BronzeTeamFixture valid and invalid records",
-        # ),
-        # pytest.param(
-        #     TEAM_INPUT_DF,
-        #     BronzeTeams,
-        #     TEAM_EXPECTED_RESULT,
-        #     id="Ensure separates BronzeTeamFixture valid and invalid records",
-        # ),
+        pytest.param(
+            EVENT_INPUT_DF,
+            BronzeEvent,
+            EVENT_EXPECTED_RESULT,
+            id="Ensure separates BronzeEventFixture valid and invalid records",
+        ),
+        pytest.param(
+            STATS_INPUT_DF,
+            BronzeStats,
+            STATS_EXPECTED_RESULT,
+            id="Ensure separates BronzeStatsFixture valid and invalid records",
+        ),
+        pytest.param(
+            FIXTURE_INPUT_DF,
+            BronzeFixture,
+            FIXTURE_EXPECTED_RESULT,
+            id="Ensure separates BronzeFixtureFixture valid and invalid records",
+        ),
     ],
 )
 def test_generic_bronze_transform(inp_df, validator_cls, expected_result):
