0b01603fb0e77f6dc819d2ff8e2adafb33b031d2
Chris Matthews <c_matthews90@live.com>
2026-02-24 20:42:10 +0000

    feat: simpler raw layer

--- src/football_pipeline/stages/raw/raw_pipe.py
+++ src/football_pipeline/stages/raw/raw_pipe.py
@@ -1,15 +1,10 @@
 import argparse  # parses command-line args
-import os
 
 from football_pipeline.adapters.fs_wrapper import FSLocal
 from football_pipeline.adapters.io_wrapper import FileType, IOWrapper
 from football_pipeline.adapters.logger import RealLogger
 from football_pipeline.adapters.repo import Repo
 from football_pipeline.adapters.time import new_guid, time_now
-from football_pipeline.stages.raw.transform import (
-    add_ingestion_columns,
-    convert_data_to_df,
-)
 
 
 def run_raw_layer(config_path: str, repo: Repo) -> dict[str, bool]:
@@ -19,7 +14,6 @@ def run_raw_layer(config_path: str, repo: Repo) -> dict[str, bool]:
         return {"valid_config": False}
 
     save_path = config.get("save_root", "./")
-    batch_guid = repo.guid_func()
     date_time_str = repo.time_func()
     successes = {}
 
@@ -29,23 +23,11 @@ def run_raw_layer(config_path: str, repo: Repo) -> dict[str, bool]:
         repo.logger.info(msg="attempting to read", path=path, keys=keys)
         api_response = repo.io.read(path, FileType.FOOTBALL_API)
         repo.logger.info(msg="successfully received message.", path=path, keys=keys)
-        res = convert_data_to_df(api_response, path, keys)
 
-        if isinstance(res, list):
-            repo.logger.error(df=res, err="Retrieved object was a list.")
-            raise ValueError(res)
-
-        res = {
-            k: add_ingestion_columns(v, batch_guid, date_time_str)
-            for k, v in res.items()
-        }
-
-        for k, v in res.items():
-            file_save_path = f"{save_path}/{k}/{date_time_str}.parquet"
-            repo.fs.create_dir(os.path.dirname(file_save_path))
-            successes[file_save_path] = repo.io.write(
-                file_save_path, v, FileType.PARQUET
-            )
+        file_save_path = f"{save_path}/{k}/{date_time_str}.json"
+        successes[file_save_path] = repo.io.write(
+            file_save_path, api_response, FileType.JSON
+        )
 
     return successes
 
