f0a9c0bb38993e907d524271b644499a3514c9b6
Chris Matthews <c_matthews90@live.com>
2026-03-18 20:58:46 +0000

    fix: add filename to silver_teams

--- warehouse/models/silver/silver_teams.sql
+++ warehouse/models/silver/silver_teams.sql
@@ -1,9 +1,11 @@
 {{ config(materialized="incremental") }}
 
 with
-    unnested_teams as (select unnest(teams) as teams from {{ ref("bronze_statics") }}),
+    unnested_teams as (
+        select unnest(teams) as teams, filename from {{ ref("bronze_statics") }}
+    ),
 
-    exploded_again as (select teams.* from unnested_teams)
+    exploded_again as (select teams.*, filename from unnested_teams)
 
 select *
 from exploded_again
